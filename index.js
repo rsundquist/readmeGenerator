2720e5122a1644ef995f39a2bfec617e745237a9
Rebecca Sundquist <rsundquist629@gmail.com>
2021-10-07 22:03:49 -0400

    added inquirer

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-inquirer.prompt(
-    {
-        type: 'input',
-        message="What is the project title?",
-        name:'title',
-        validate: (value)=>{ if(value){return true} else {return 'Need a value to continue'}},
-    },
+const readMeData = (input) => `
+# Project Title
+${input.projectTitle}
+## Installation
+${input.installation}    
+## Usage/Examples
+${input.usage}
+## Contributing
+${input.contributing}
+## Running Tests
+${input.runningTests}
+## Feedback
+If you have any feedback, please reach out to us at ${input.feedback}
+`
 
+inquirer
+  .prompt([
     {
-        type: 'input',
-        message="What packages do you need to install?",
-        name:'Installation',
-        validate: (value)=>{ if(value){return true} else {return 'Need a value to continue'}},
+      type: 'input',
+      name: 'projectTitle',
+      message: "What is A brief description of what this project does?",
+    },
+    {
+      type: 'input',
+      name: 'packages',
+      message: 'What packages do you need to install?',
+      
     },
     {
         type: 'input',
-        message="Installation instructions for app?",
-        name:'Instructions',
-        validate: (value)=>{ if(value){return true} else {return 'Need a value to continue'}},
-    },
-    {
-        type: 'input',
-        message="How do you use your app?",
-        name:'Use of App',
-        validate: (value)=>{ if(value){return true} else {return 'Need a value to continue'}},
-    },
-    {
+        name: 'usage',
+        message: 'What are some usage examples?',
+        
+      },
+      {
         type: 'list',
-        message="What license did you use?",
-        name:'license',
-        choices:['The MIT License', 'The GPL License', 'Apache License', 'NA'],
-        validate: (value)=>{ if(value){return true} else {return 'Need a value to continue'}},
-    },
-    {
+        name: 'license',
+        message: 'Which license did you use?',
+        choices:['The MIT License', 'The GPL License', 'Apache License', "NA"],
+        
+      },
+      {
         type: 'input',
-        message="Github Username",
-        name:'Username',
-        validate: (value)=>{ if(value){return true} else {return 'Need a value to continue'}},
-    },
-    {
+        name: 'runningTests',
+        message: 'What command do you run to test?',
+        
+      },
+      {
         type: 'input',
-        message="Email",
-        name:'Email',
-        validate: (value)=>{ if(value){return true} else {return 'Need a value to continue'}},
-    },
+        name: 'feedback',
+        message: 'What email address is used for feedback?',
+      },
     
+  ])
+  .then((input) => {
+    const readMe = readMeData(input)
+    const filename = `README.md`;
 
-)
+    fs.writeFile(filename, readMe, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
